861069ffb7f3c6261de57ab55f7afad143d0a8ba
feimpraim <fredickimpraim@gmail.com>
2021-03-11 20:40:21 -0700

    second commit

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,5 +5,65 @@ cost crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
 
-    username
-})
+    username:{
+        type: String,
+        trim: true,
+        required: true,
+        maxLength: 32,
+        unique: true,
+        index: true,
+        lowecase:true,
+    },
+
+    name:{
+        type: String,
+        trim: true,
+        required: true,
+        maxLength: 32,
+        
+    },
+
+    email:{
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        lowecase:true,
+        
+    },
+    profile:{
+        type: String,
+        required: true,
+        
+        
+    },
+
+    hashed_password:{
+        type: String,
+        required: true,
+        
+        
+    },
+    salt: String,
+    about:{
+        type: String
+    },
+    role:{
+        type: Number,
+        trim: true,
+    },
+
+    photo:{
+        data:Buffer,
+        contentType: String
+    },
+
+    resetPasswordLink: {
+
+        data: String,
+        default:'',
+    }
+
+},{timestamps: true})
+
+module.exports = mongoose.model('User',userSchema)
